a98698429a51a442e416aacacb4bd3269611c88f
carob98 <caro.bon98@gmail.com>
2025-07-23 17:58:51 +0200

    Update file

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Lo slash = root (pagina principale)
+// Callback function
+
+// dominio = http://127.0.0.1:8000
+
+// Fa il controllo sull'uri
 Route::get('/', function () {
     return view('welcome');
+});
+
+// url = http://127.0.0.1:8000/ciccio
+Route::get('/ciccio', function () {
+    return 'ciccio';
+});
+
+// url = http://127.0.0.1:8000/topolino
+Route::get('/topolino', function () {
+    return view('topolino');
 });
